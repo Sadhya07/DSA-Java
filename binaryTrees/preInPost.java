db8f43b5f3b9cb7b88c4386da82a78f6f15fd497
Sadhya07 <sadhyaguptasg@gmail.com>
2025-07-29 23:34:24 +0530

    Added some code

--- binaryTrees/preInPost.java
+++ binaryTrees/preInPost.java
@@ -1,5 +1,5 @@
 package binaryTrees;
-
+import java.util.*;
 public class preInPost {
     public static class Node{
         int val;
@@ -10,7 +10,7 @@ public class preInPost {
             this.val = val;
         }
     }
-        public static int height(Node root){
+    public static int height(Node root){
         if(root == null || root.left == null && root.right == null) return 0;
         return 1 + Math.max(height(root.left), height(root.right));
     }
@@ -38,9 +38,24 @@ public class preInPost {
     }
     public static void nthLevel(Node root, int n){
         if(root == null) return;
-        if(n==1) System.out.print(root.val);
+        if(n==1){
+            System.out.print(root.val + " ");
+            return;
+        } 
         nthLevel(root.left, n-1);
         nthLevel(root.right, n-1);
+    }
+    public static void bfs(Node root){ //Iterative
+        Queue<Node> q=new LinkedList<>();
+        if(root !=null) q.add(root);
+        while(q.size() >0){
+            Node temp = q.peek();
+            if(temp.left !=null) q.add(temp.left);
+            if(temp.right != null) q.add(temp.right);
+            System.out.println(temp.val +" ");
+            q.remove();
+        }
+
     }
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -63,10 +78,10 @@ public class preInPost {
         // postorder(root);
         // nthLevel(root,1);
         int level = height(root) +1;
-        for(int i=1; i<=level; i++){
-            nthLevel(root, i);
-            System.out.println();
-        }
+        for(int i=1; i<=level; i++){  //logn
+            nthLevel(root, i); // O(n)
+            System.out.println(); 
+        } //T.C : O(nlogn)
 
  
         
